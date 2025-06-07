ad3e4d5a9b18e45a94e748db7100bcd85d809819
clarcksp <ronaldosc86@gmail.com>
2025-06-07 07:00:06 -0300

    Corrigi Bugs
    Corrigi Bugs

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 8080
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
